b4e8d0c104fe1efc04f69a4ea9e7234c842137d4
Laura Vogelaar <vogelaar@ensta.fr>
2013-02-24 23:30:39 +0100

    Better commenting, modified plotting (plots trajectories in between distribution update as well), renamed previous_experience to learning_history since that's what it really is, and added previous_experience which contains data useful to task splitting.

--- skills/Skill.m
+++ skills/Skill.m
@@ -4,7 +4,10 @@ classdef Skill
     distributions;
     rollout_buffer; %rollout.dmp_parameters_sample, rollout.sensation, rollout.cost
     i_update;
-    previous_experience;
+    previous_experience; %contains precept, sample, cost, and i_update
+    
+    learning_history; %same structure as used in other locations
+    
     % later previous_experience; %update number, rollout, current distribution
     % later subskills;
   end
@@ -18,6 +21,7 @@ classdef Skill
       % given the sensation and percept
       holds = true;      
     end
+   
     
     function obj = solve_task_instance(obj,task_instance, task_solver, percept)
       % Adds task_instance to the task_instance_buffer (useful when
@@ -31,39 +35,67 @@ classdef Skill
       %     - put results in previous experience
       
       K=20;
-      samples = generate_samples(obj.distributions,1,0);
       plot_me = 0;
       n_dofs = length(obj.distributions);
 
       sensation = [];
-      
+      % check precondition
       if precondition_holds(obj, sensation, percept)
         obj.i_update = obj.i_update + 1;
+       
+        %sample dmp parameters from distribution (1 at a time)
+        samples = generate_samples(obj.distributions,1,0);
+        
+        %execute skill, record relevant cost variables
         cost_vars = task_solver.perform_rollouts(task_instance,samples);
+        
+        %determine cost, given the task and relevant cost variables
         costs = task_instance.cost_function(task_instance,cost_vars);
-        %costs = task_instance.perform_rollouts(task_instance,samples,plot_me);
+        
+        %put results in rollout buffer
         rollout.dmp_parameters_distributions = obj.distributions;
         rollout.dmp_parameters_sample = samples;
         rollout.cost = costs;
         obj.rollout_buffer{end+1} = rollout;
+        
+        %if rollout buffer is full, update distributions and clear buffer
         if(length(obj.rollout_buffer)>K)
+          %reshape the rollouts to work nicely with the
+          %update_distributions function
           for ii = 1:length(obj.rollout_buffer)
             s(:,ii,:) = squeeze(obj.rollout_buffer{ii}.dmp_parameters_sample);
             c(ii,:) = obj.rollout_buffer{ii}.cost;
           end
+          
+          %update the distributions
           [obj.distributions summary] = update_distributions(obj.distributions,s,c);
-          if isempty(obj.previous_experience)
-            obj.previous_experience = summary;
+          
+          %add to learning history to make a nice plot
+          if isempty(obj.learning_history)
+            obj.learning_history = summary;
           else
-            obj.previous_experience(end+1) = summary;
+            obj.learning_history(end+1) = summary;
           end
-          plot_me = 1;
+          plot_me = 1; %turn plotting on for the distributions
+          
+          %empty the rollout buffer
           obj.rollout_buffer = [];
         end
         
+        %add to previous history!
+        previous.percept = percept;
+        previous.sample = samples;
+        previous.cost = costs;
+        previous.i = obj.i_update;
+        %need to do this to initiallize as a struct array
+        if isempty(obj.previous_experience)
+          obj.previous_experience = previous;
+        else
+          obj.previous_experience(end+1) = previous;
+        end
      
-     % Plotting
-     if(plot_me)
+        % Plotting
+     
          figure(1)
          
          % Very difficult to see anything in the plots for many dofs
@@ -75,24 +107,22 @@ classdef Skill
            task_solver.plot_rollouts(gca,task_instance,cost_vars)
            title('Visualization of roll-outs')
          end
-         
-         % Plot learning histories
-         if (obj.i_update>0 && ~isempty(obj.previous_experience))
-           plotlearninghistory(obj.previous_experience);
-           if (isfield(task_instance,'plotlearninghistorycustom'))
-             figure(11)
-             task_instance.plotlearninghistorycustom(obj.previous_experience)
+         if(plot_me)
+           % Plot learning histories
+           if (obj.i_update>0 && ~isempty(obj.learning_history))
+             plotlearninghistory(obj.learning_history);
+             if (isfield(task_instance,'plotlearninghistorycustom'))
+               figure(11)
+               task_instance.plotlearninghistorycustom(obj.learing_history)
+             end
            end
+           
+           plot_me = 0;
          end
-         
-         plot_me = 0;
-     end
+ 
+      end %end if ( precondition_holds )
       
-      
-        
-      end
-      
-    end
+    end %end solve_task_instance
     
     function print(obj)
       %Prints out a list of current subskills
@@ -140,7 +170,7 @@ function obj = Skill_test_function
    obj.rollout_buffer = [];
    obj.i_update = 0;
    
-   while obj.i_update < 400
+   while obj.i_update < 100
      if(rand(1)>0.5)
        task = task_viapoint([0.4 0.7],0.3);
        percept = 1;
