ad0dd8ea3e8a9f0a4b5e8320d07377f135c16741
Laura Vogelaar <vogelaar@ensta.fr>
2013-02-24 22:33:43 +0100

    Added plotting of distributions within the Skill class, but is now very slow.

--- skills/Skill.m
+++ skills/Skill.m
@@ -4,6 +4,7 @@ classdef Skill
     distributions;
     rollout_buffer; %rollout.dmp_parameters_sample, rollout.sensation, rollout.cost
     i_update;
+    previous_experience;
     % later previous_experience; %update number, rollout, current distribution
     % later subskills;
   end
@@ -18,7 +19,7 @@ classdef Skill
       holds = true;      
     end
     
-    function obj = solve_task_instance(obj,task_instance, task_solver)
+    function obj = solve_task_instance(obj,task_instance, task_solver, percept)
       % Adds task_instance to the task_instance_buffer (useful when
       % executing in batch mode)
 
@@ -30,18 +31,13 @@ classdef Skill
       %     - put results in previous experience
       
       K=20;
-      plot_me = 1;
       samples = generate_samples(obj.distributions,1,0);
-      
+      plot_me = 0;
       n_dofs = length(obj.distributions);
-      %for i_dof = 1:n_dofs
-      %  samples(i_dof,:) = mvnrnd(obj.distributions(i_dof).mean, obj.distributions(i_dof).covar,1);
-      %end
-      
+
       sensation = [];
-      precept = [];
       
-      if precondition_holds(obj, sensation, precept)
+      if precondition_holds(obj, sensation, percept)
         obj.i_update = obj.i_update + 1;
         cost_vars = task_solver.perform_rollouts(task_instance,samples);
         costs = task_instance.cost_function(task_instance,cost_vars);
@@ -56,31 +52,46 @@ classdef Skill
             c(ii,:) = obj.rollout_buffer{ii}.cost;
           end
           [obj.distributions summary] = update_distributions(obj.distributions,s,c);
+          if isempty(obj.previous_experience)
+            obj.previous_experience = summary;
+          else
+            obj.previous_experience(end+1) = summary;
+          end
+          plot_me = 1;
+          obj.rollout_buffer = [];
         end
         
-        plot_me = 1;
      
      % Plotting
-     if (plot_me)
-       figure(1)
-       if (obj.i_update==0), clf; end
-       
-       % Very difficult to see anything in the plots for many dofs
-       plot_n_dofs = min(n_dofs,3);
-       
-       % Plot rollouts if the plot_rollouts function is available
-       if (isfield(task_solver,'plot_rollouts'))
+     if(plot_me)
+         figure(1)
          
-         task_solver.plot_rollouts(gca,task_instance,cost_vars)
-         title('Visualization of roll-outs')
-       end
-
+         % Very difficult to see anything in the plots for many dofs
+         plot_n_dofs = min(n_dofs,3);
+         
+         % Plot rollouts if the plot_rollouts function is available
+         if (isfield(task_solver,'plot_rollouts'))
+           subplot(plot_n_dofs,4,1:4:plot_n_dofs*4)
+           task_solver.plot_rollouts(gca,task_instance,cost_vars)
+           title('Visualization of roll-outs')
+         end
+         
+         % Plot learning histories
+         if (obj.i_update>0 && ~isempty(obj.previous_experience))
+           plotlearninghistory(obj.previous_experience);
+           if (isfield(task_instance,'plotlearninghistorycustom'))
+             figure(11)
+             task_instance.plotlearninghistorycustom(obj.previous_experience)
+           end
+         end
+         
+         plot_me = 0;
      end
-        
+      
+      
         
       end
-      % if rollout buffer is full
-      % update the distributions!
+      
     end
     
     function print(obj)
@@ -122,21 +133,23 @@ function obj = Skill_test_function
      distributions(i_dof).mean = ones(1,n_dims);
      distributions(i_dof).covar = 5*eye(2);
    end
-
+   
    g = [1.0 1.0];
    y0 = [0.0 0.0];
    obj = Skill('Test skill',distributions);
    obj.rollout_buffer = [];
    obj.i_update = 0;
    
-   while obj.i_update < 200
+   while obj.i_update < 400
      if(rand(1)>0.5)
        task = task_viapoint([0.4 0.7],0.3);
+       percept = 1;
      else
        task = task_viapoint([0.7 0.4],0.3);
+       percept = 0;
      end
      task_solver = task_viapoint_solver_dmp(g,y0,0);
-     obj = obj.solve_task_instance(task,task_solver);
+     obj = obj.solve_task_instance(task,task_solver,percept);
      
    end
 
