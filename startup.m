1f78bc8afa8c13c1fa24de5d151209912a0c916e
Freek Stulp <stulp@clmc.usc.edu>
2013-07-09 16:01:08 +0200

    Startup script

---
+++ startup.m
@@ -0,0 +1,8 @@
+%--------------- Startup ---------------
+addpath(genpath('dynamicmovementprimitive'))
+addpath(genpath('evolutionaryoptimization'))
+addpath(genpath('fileio'))
+addpath(genpath('tasks'))
+addpath(genpath('main'))
+addpath(genpath('skills'))
+addpath(genpath('vrep/programming'))
